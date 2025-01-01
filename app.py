9eea91898fa08e8fa1963e04eac8ce0bf9b5f7da
polashds <polashtechai@gmail.com>
2025-01-01 14:01:37 +0600

    model trainer

--- app.py
+++ app.py
@@ -2,8 +2,8 @@ from src.mlproject.logger import logging
 from src.mlproject.exception import CustomException
 from src.mlproject.components.data_ingestion import DataIngestion
 from src.mlproject.components.data_ingestion import DataIngestionConfig
-#from src.mlproject.components.data_transformation import DataTransformationConfig,DataTransformation
-#from src.mlproject.components.model_tranier import ModelTrainerConfig,ModelTrainer
+from src.mlproject.components.data_transformation import DataTransformationConfig,DataTransformation
+from src.mlproject.components.model_tranier import ModelTrainerConfig,ModelTrainer
 
 import sys
 
@@ -16,8 +16,8 @@ if __name__=="__main__":
 
     try:
         #data_ingestion_config=DataIngestionConfig()
-        data_ingestion=DataIngestion()
-        train_data_path,test_data_path=data_ingestion.initiate_data_ingestion()
+        #data_ingestion=DataIngestion()
+        #train_data_path,test_data_path=data_ingestion.initiate_data_ingestion()
 
         #data_transformation_config=DataTransformationConfig()
         data_transformation=DataTransformation()
